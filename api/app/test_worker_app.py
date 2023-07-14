bb76540b997c61b6df7340d924c2ebc5cdd7da7b
Nimamoh <nimamoh@nimamoh.net>
2023-07-14 11:30:58 +0200

    [CHORE] example in test_worker_app.py

--- api/app/test_worker_app.py
+++ api/app/test_worker_app.py
@@ -1,6 +1,7 @@
 """Une application celery de test qui peut être utilisé comme worker"""
 
 from celery import Celery
+from celery import current_task
 import time
 
 from .Settings import Settings
@@ -9,16 +10,9 @@ settings = Settings()  # type: ignore
 celery = Celery(
     "test_app", broker=settings.broker, backend=settings.backend, result_extended=True
 )
-# celery = Celery('test_app', broker=settings.broker, backend=settings.backend)
-
 
 @celery.task
 def add(x, y):
-    return x + y
-
-
-@celery.task
-def child_add(x, y):
     return x + y
 
 
@@ -32,7 +26,14 @@ def ohlong(seconds: int = 5):
     time.sleep(seconds)
 
 
+# region: task with parent
 @celery.task
 def add_n_times(n=5, a=1, b=2):
+    curr_taskid = current_task.request.id
     for _ in range(n):
-        child_add.delay(a, b)
+        child_add.delay(a, b, ocm_parent_id=curr_taskid)
+
+@celery.task
+def child_add(x, y, ocm_parent_id = None):
+    return x + y
+# endregion
