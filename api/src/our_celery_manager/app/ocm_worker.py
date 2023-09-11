24b1f6bea59ca49d50fae391a49ae51b4a54acdb
Nimamoh <nimamoh@nimamoh.net>
2023-09-11 17:38:30 +0200

    [FEAT] better worker

--- api/src/our_celery_manager/app/ocm_worker.py
+++ api/src/our_celery_manager/app/ocm_worker.py
@@ -1,4 +1,4 @@
-"""Un worker pour les tâches OCM"""
+"""A worker for our-celery-manager related tasks"""
 
 import logging
 from celery.states import FAILURE
@@ -21,10 +21,18 @@ logger = logging.getLogger(__name__)
 def ocm_cleanup_backend(being_older_than_days: int, only_failures=True):
     """Task similar to celery cleanup backend except it accepts more options"""
 
+    result_backend_addr = settings.hiding_passwords().backend
+
+    logger.info(
+        f"Cleaning tasks in {result_backend_addr} which are older than {being_older_than_days} days"
+        ". Only cleaning failures." if only_failures else ""
+    )
+
     if only_failures is False:
         raise NotImplementedError("Only 'only_failures' as True is supported")
 
     older_than = datetime.now() - timedelta(being_older_than_days)
+    logger.debug(f"Cleaning tasks which date_done are older than {older_than}")
 
     stmt = (
         delete(TaskExtended)
@@ -32,7 +40,9 @@ def ocm_cleanup_backend(being_older_than_days: int, only_failures=True):
         .where(TaskExtended.date_done < older_than)
     )
     session = celery.backend.ResultSession()
-    session.execute(stmt)
+    result = session.execute(stmt)
+    logger.info(f"Successfully deleted {result.rowcount} rows")
+    session.commit()
 
 
     
